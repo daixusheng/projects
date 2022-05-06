c51159c33f4574d3004af049757c3fe88257c5ee
daixusheng <204933796@qq.com>
2022-05-06 20:51:36 +0800

    util.js

--- util.js
+++ util.js
@@ -1,6 +1,4 @@
 // 判断当年是否为闰年
-// 闰年（leap year）二月 29 天（多一天）
-// 平年（common year）二月 28 天
 function isLeapYear(year) {
   let flag = false;
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
@@ -8,4 +6,3 @@ function isLeapYear(year) {
   }
   return flag;
 }
-// console.log(isLeapYear(2004));
